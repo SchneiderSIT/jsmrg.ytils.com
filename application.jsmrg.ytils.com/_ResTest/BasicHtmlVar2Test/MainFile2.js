343f347e2bad9860af75d480d130bbc809cda8e1
Kim Schneider <>
2020-10-11 18:56:41 +0200

    Adds BasicHtmlVar2Test and assets.

---
+++ application.jsmrg.ytils.com/_ResTest/BasicHtmlVar2Test/MainFile2.js
@@ -0,0 +1,18 @@
+/*global $ */
+/*global window */
+/*global alert */
+/*jslint browser: true */
+/*jslint white: true */
+/*jslint long: true */
+(function(){
+
+    "use strict";
+    if (!window.YTILS) {
+        
+        var foo = "";
+        var bar = "";
+        var html = "/**jsmrg htmlvar    escdoublequotes  lb2space  HtmlVar2a.html  %d%foo %d%bar */";
+        var html2 = '/**jsmrg htmlvar    escsinglequotes HtmlVar2b.html  %s%foo %s%bar */';
+    }
+
+}());
---
+++ test.jsmrg.ytils.com/IntegrationTest/BasicHtmlVar2Test .cs
@@ -0,0 +1,21 @@
+using application.jsmrg.ytils.com.lib;
+using NUnit.Framework;
+
+namespace test.jsmrg.ytils.com.IntegrationTest
+{
+    public class BasicHtmlVar2Test
+    {
+        [Test]
+        public void TestBasicHtmlVar2()
+        {
+            var programRunner = new ProgramRunner(new [] { "_ResTest/BasicHtmlVar2Test/MainFile2.js", "_ResTest/BasicHtmlVar2Test/MainFile2.out.js" });
+            var result = programRunner.Run();
+            var output = programRunner.JsMrgOutput;
+            
+            Assert.AreEqual(ProgramRunnerExit.Done, result);
+            Assert.True(output.Contains("<div class=\\\"someContainer\\\">"));
+            Assert.True(output.Contains("<div class=\\\"\" + foo + \"\\\">\" + bar + \"</div>"));
+            Assert.IsTrue(output.Contains("'<div class=\\'someContainer\\'><div class=\\'' + foo + '\\'>' + bar + '</div></div>';"));
+        }
+    }
+}
