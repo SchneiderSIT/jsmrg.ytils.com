f00805b4c6ee05e6bb9a1fa9cf61ccf8affafdcc
kim <kim@schneider.ws>
2023-08-13 11:10:45 +0200

    Upgrades to .NET 6 and cosmetics.

--- application.jsmrg.ytils.com/Lib/App.cs
+++ application.jsmrg.ytils.com/Lib/App.cs
@@ -1,9 +1,9 @@
 namespace application.jsmrg.ytils.com.lib
 {
-    public class App
+    public abstract class App
     {
-        public const string Version = "0.8.5";
-        public const string Date = "February 2021";
+        public const string Version = "0.9";
+        public const string Date = "August 2023";
         public const string Website = "ytils.com/jsmrg";
         public const string FullDocumentationUri = "https://www.ytils.com/jsmrg";
     }
