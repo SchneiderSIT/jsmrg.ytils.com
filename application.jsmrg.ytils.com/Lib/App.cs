27af8bbaee57f73f28cd05b6419c537eb76cf08e
kim <kim@schneider.ws>
2025-01-24 20:41:46 +0100

    Fixes correct version upcount.

--- application.jsmrg.ytils.com/Lib/App.cs
+++ application.jsmrg.ytils.com/Lib/App.cs
@@ -2,7 +2,7 @@ namespace application.jsmrg.ytils.com.lib
 {
     public abstract class App
     {
-        public const string Version = "0.14";
+        public const string Version = "0.10";
         public const string Date = "January 2025";
         public const string Website = "ytils.com/jsmrg";
         public const string FullDocumentationUri = "https://www.ytils.com/jsmrg";
