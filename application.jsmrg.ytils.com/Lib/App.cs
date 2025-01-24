4c3d9cc4b62d2d87bf4d09246a5737d51b89d847
kim <kim@schneider.ws>
2025-01-24 20:35:39 +0100

    Update to .NET8.

--- application.jsmrg.ytils.com/Lib/App.cs
+++ application.jsmrg.ytils.com/Lib/App.cs
@@ -2,8 +2,8 @@ namespace application.jsmrg.ytils.com.lib
 {
     public abstract class App
     {
-        public const string Version = "0.9";
-        public const string Date = "August 2023";
+        public const string Version = "0.14";
+        public const string Date = "January 2025";
         public const string Website = "ytils.com/jsmrg";
         public const string FullDocumentationUri = "https://www.ytils.com/jsmrg";
     }
